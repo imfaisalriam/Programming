3e09650d88ceda61860ec9356ba105be83061ef0
Faisal-Ahmed-Riam <smfaisalriam@gmail.com>
2024-05-12 14:09:22 +0600

    dd

---
+++ CPP/First.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#include <stdio.h>
+#define ll long long int
+#define yes cout << "Yes" << endl
+#define no cout << "No" << endl
+using namespace std;
+int main()
+{
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+       ll n;
+       cin>>n;
+       for (int i = 0; i < n; i++)
+       {
+           cout<<i+1<<endl;
+       }
+       
+       while (n--)
+       {
+           cout<<n<<endl;
+       } 
+    }
+}
