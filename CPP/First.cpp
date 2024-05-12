ebf67c7bf99af6960f4952320c3b796e14ef259a
Faisal-Ahmed-Riam <smfaisalriam@gmail.com>
2024-05-12 11:25:24 +0600

    eadded filr

--- CPP/First.cpp
+++ CPP/First.cpp
@@ -1,24 +1,8 @@
-#include<iostream>
-
+#include<bits/stdc++.h>
+using namespace std;
 int main()
-{   /*
-    int eliphant_count;
-    int lion_count{};
-    int cat_count{10};
-    int dog_count{15};
-
-    int domestic_animals{cat_count + dog_count};
-    std::cout << "Eliphant count :" << eliphant_count << std::endl;
-    std::cout << "Lion count :" << lion_count << std::endl;
-    std::cout << "Cat count :" << cat_count << std::endl;
-    std::cout << "Dog count :" << dog_count << std::endl;
-    std::cout << "Domestic animals :" << domestic_animals << std::endl;
-    */
-
-     signed int number{4294967296};
-      //unsigned int number2{10};
-
-      std::cout<<number<<' '<<std::endl;
+{  
+  
 
     return 0;
 
